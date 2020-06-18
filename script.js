1b4593649514fb35db6d762761cb1fc79db90b64
keobong2011999 <keobong2011999@gmail.com>
2020-06-18 22:01:36 +0700

    thay doi thu hai

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function tinh() {
     let so1 = document.getElementById("so1").Value;
     let so2 = document.getElementById("so2").Value;
-    let cong = document.getElementById("tong_day_la_thay_doi_so_1");
+    let cong = document.getElementById("tong_day_la_thay_doi_so_2");
     let t = Number(so1) + Number(so2)
     cong.innerHTML = "tổng là:" + t
 }
