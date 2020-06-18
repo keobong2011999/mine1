1ad213943e3b6371ae4b76894bfad5a2c90f026c
keobong2011999 <keobong2011999@gmail.com>
2020-06-18 22:10:50 +0700

    thaydoi888 999

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 function tinh() {
-    let so1 = document.getElementById("so1").Value;
-    let so2 = document.getElementById("so2").Value;
-    let cong = document.getElementById("tong_day_la_thay_doi_so_2");
+    let so1 = document.getElementById("so1_888").Value;
+    let so2 = document.getElementById("so2__999").Value;
     let t = Number(so1) + Number(so2)
     cong.innerHTML = "tổng là:" + t
 }
