bed11966d2836bac198bfd91fc0e6c980799d8e2
keobong2011999 <keobong2011999@gmail.com>
2020-06-18 21:52:45 +0700

    init commit

---
+++ script.js
@@ -0,0 +1,7 @@
+function tinh() {
+    let so1 = document.getElementById("so1").Value;
+    let so2 = document.getElementById("so2").Value;
+    let cong = document.getElementById("tong");
+    let t = Number (so1)+ Number(so2)
+    cong . innerHTML ="tổng là:" +t
+}
---
+++ vankhoi2.js
@@ -0,0 +1,2 @@
+let a = 'toidicode.com';
+console.log(a);
